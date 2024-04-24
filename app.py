8bc96188a85fd4e40815a3c40d4e832a1f2a3f68
reondaze-a <75068289+reondaze-a@users.noreply.github.com>
2024-04-24 10:45:35 -0400

    Updated app.py and EDA Notebook
    adding a few lines of code to intialize app.py as well as making new graphs on the Notebook

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+
+st.header('Calculate pokemon stats')
+
+level = st.slider(1, 100, 5)
+start_button = st.button('Calculate')
+
+if start_button:
